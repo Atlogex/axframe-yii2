7951035f325f88ca938c0989f0d2a87ee6e8af40
Atlogex <atlogex@gmail.com>
2017-12-22 20:05:57 +0600

    Add Support Themes

--- framework_yii2/frontend/assets/AppAsset.php
+++ framework_yii2/frontend/assets/AppAsset.php
@@ -9,15 +9,22 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@app/themes/default/assets/';
+//    public $basePath = '@webroot';
+//    public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'site.css',
     ];
     public $js = [
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
 }
+
+//    public $sourcePath = '@app/themes/axy2shop/assets/';
+//    public $sourcePath = '@webroot/css';
+//    public $basePath = '@webroot/css';
+//    public $baseUrl = '@web/themes/axy2shop/assets/';
--- framework_yii2/frontend/config/main.php
+++ framework_yii2/frontend/config/main.php
@@ -1,49 +1,62 @@
 <?php
 $params = array_merge(
-    require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
+	require __DIR__ . '/../../common/config/params.php',
+	require __DIR__ . '/../../common/config/params-local.php',
+	require __DIR__ . '/params.php',
+	require __DIR__ . '/params-local.php'
 );
 
 return [
-    'id' => 'app-frontend',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'frontend\controllers',
-    'components' => [
-        'request' => [
-            'csrfParam' => '_csrf-frontend',
-        ],
-        'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
-        ],
-        'session' => [
-            // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
-    ],
-    'params' => $params,
+	'id' => 'app-frontend',
+	'basePath' => dirname(__DIR__),
+	'bootstrap' => ['log'],
+	'controllerNamespace' => 'frontend\controllers',
+	'components' => [
+		'request' => [
+			'csrfParam' => '_csrf-frontend',
+		],
+		'user' => [
+			'identityClass' => 'common\models\User',
+			'enableAutoLogin' => true,
+			'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+		],
+		'session' => [
+			// this is the name of the session cookie used for login on the frontend
+			'name' => 'advanced-frontend',
+		],
+		'log' => [
+			'traceLevel' => YII_DEBUG ? 3 : 0,
+			'targets' => [
+				[
+					'class' => 'yii\log\FileTarget',
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+		'errorHandler' => [
+			'errorAction' => 'site/error',
+		],
+		/*
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName' => false,
+			'rules' => [
+			],
+		],
+		*/
+		'view' => [
+			'theme' => [
+				'basePath' => '@frontend/themes/default',
+				'baseUrl' => '@frontend/themes/default',
+				'pathMap' => [
+					'@app/views' => '@frontend/themes/default',
+					'@app/modules' => '@frontend/themes/default/modules',
+					'@app/widgets' => '@app/themes/default/widgets',// <-- !!!
+//					'@app/widgets' => '@app/themes/axy2shop/widgets',
+//					'@app/views/widgets' => '@frontend/themes/axy2shop/widgets',
+				],
+			],
+		],
+	],
+	'params' => $params,
 ];
