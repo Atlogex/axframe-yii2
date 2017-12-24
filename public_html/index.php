baf4c301d26afc50081aaafad6d00ec73bf16493
Atlogex <atlogex@gmail.com>
2017-12-25 00:34:05 +0600

    Main composer - Update dependencies

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../framework_yii2/vendor/autoload.php');
+require(__DIR__ . '/../framework_yii2/vendor/autoload.php'); // todo - Move Vendor in root dir?
 require(__DIR__ . '/../framework_yii2/vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../framework_yii2/common/config/bootstrap.php');
 require(__DIR__ . '/../framework_yii2/frontend/config/bootstrap.php');
